a1d2c58e39f44e9b504a5ac7a2d59ff8d6a12282
mafers <fernanda.suarez@alumnos.upm.es>
2023-05-25 15:10:43 +0200

    Missed file

--- src/py_vision/py_vision/py_vision.py
+++ src/py_vision/py_vision/py_vision.py
@@ -2,6 +2,7 @@ from interfaces.srv import IdentifyPiece
 
 import rclpy
 from rclpy.node import Node
+from .main import Identification
 
 
 class PyVision(Node):
@@ -12,9 +13,10 @@ class PyVision(Node):
 
     def add_two_ints_callback(self, request, response):
         self.get_logger().info('Incoming request')
-
+        
+        response.piece.piece_id = Identification()
+        response.piece.piece_orientation = 1
         return response
-
 
 def main(args=None):
     rclpy.init(args=args)
