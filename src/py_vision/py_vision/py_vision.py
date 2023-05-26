16ac942ce2539eeeb3dfc2e381e7358069e8cbf6
sr-pdm <@sr-pdm>
2023-05-26 15:03:38 +0200

    Added locatePiece to py_vision

--- src/py_vision/py_vision/py_vision.py
+++ src/py_vision/py_vision/py_vision.py
@@ -1,4 +1,6 @@
 from interfaces.srv import IdentifyPiece
+from interfaces.srv import LocatePieces
+from interfaces.msg import PiecePose
 
 import rclpy
 from rclpy.node import Node
@@ -9,13 +11,33 @@ class PyVision(Node):
 
     def __init__(self):
         super().__init__('py_vision')
-        self.srv = self.create_service(IdentifyPiece, 'py_vision/IdentifyPiece', self.add_two_ints_callback)
+        self.srv = self.create_service(IdentifyPiece, 'py_vision/identify_piece', self.identify_piece_callback)
+        self.srv = self.create_service(LocatePieces, 'py_vision/locate_pieces', self.locate_pieces_callback)
 
-    def add_two_ints_callback(self, request, response):
+    def identify_piece_callback(self, request, response):
         self.get_logger().info('Incoming request')
         
         response.piece.piece_id = Identification()
         response.piece.piece_orientation = 1
+        return response
+    
+    def locate_pieces_callback(self, request, response):
+        self.get_logger().info('Incoming request')
+
+        temp = PiecePose()
+
+        temp.piece_x = 0.02
+        temp.piece_y = 0.3
+        temp.piece_w = 1.2
+
+        response.poses.append(temp)
+
+        temp.piece_x = 0.04
+        temp.piece_y = 0.5
+        temp.piece_w = 1.3
+
+        response.poses.append(temp)
+
         return response
 
 def main(args=None):
